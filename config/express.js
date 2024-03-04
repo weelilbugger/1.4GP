5602d7595cdb94a6f287fee9698323d5af5c722b
weelilbugger <mattiaclouse@outlook.com>
2024-03-03 21:48:16 -0500

    attempt 3

---
+++ config/express.js
@@ -0,0 +1,28 @@
+const config = require('./config');
+const express = require('express');
+const morgan = require('morgan');
+const compress = require('compression');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const session = require('express-session');
+module.exports = function() {
+const app = express();
+if (process.env.NODE_ENV === 'development') {
+app.use(morgan('dev'));
+} else if (process.env.NODE_ENV === 'production') {
+app.use(compress());
+}
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+app.use(session({
+saveUninitialized: true,
+resave: true,
+secret: config.sessionSecret
+}));
+app.set('views', './app/views');
+app.set('view engine', 'ejs');
+app.use(express.static('./public'));
+require('../app/routes/index.server.routes')(app);
+return app;
+};
